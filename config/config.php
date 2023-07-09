60094914a9a1a70f556b771f043452faff3b13b3
ddos <109574396+SonuModder1@users.noreply.github.com>
2023-07-09 14:50:12 +0530

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,33 @@
+<?php
+//EN - FR
+//CONFIGURATION - CONFIG
+//SITE NAME - Nom du site
+$sitename = 'SITE NAME - Nom du site';
+// CAPS ONLY - Majuscule seulement
+
+//Bit or byte - Bit ou octet
+// US=bit and/et EU= octet
+$zone = 'US';
+
+//Data transferred or received  -  Donnée transférée ou recus
+//RX = received/recus TX = transferred/transféerée
+$datatype = 'RX';
+
+//Language - Langue
+//FR = francais/french - EN = english/anglais
+$lang = 'EN';
+
+//Name of interface - Nom de l'interface
+//Usually : 'eth0' - Generalement :'eth0'
+$interface = 'eth0';
+
+// Ip of your server
+$ip = "216.24.57.3";
+$port = "80";
+
+$l7 = "https://sakib-gamer-anti-ddos.onrender.com";
+$portt = "443";
+
+//Graph title
+$Layer4Title = "Layer 4 ==>{$ip} PORT {$port} <==";
+$Layer7Title = "Layer 7 ==>{$l7} PORT {$portt} <==";
